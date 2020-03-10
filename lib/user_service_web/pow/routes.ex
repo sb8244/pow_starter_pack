96c0bbdc9afaa8d296bfa20040bd9faa0f65e03c
Stephen Bussey <sb8244@cs.ship.edu>
2020-03-10 01:15:49 -0400

    Sign out supports redirect_to as well

--- lib/user_service_web/pow/routes.ex
+++ lib/user_service_web/pow/routes.ex
@@ -2,6 +2,8 @@ defmodule UserServiceWeb.Pow.Routes do
   use Pow.Phoenix.Routes
   alias UserServiceWeb.Router.Helpers, as: Routes
 
+  alias UserServiceWeb.Plug.RedirectTo
+
   @impl true
   def user_not_authenticated_path(conn) do
     Routes.pow_session_path(conn, :new)
@@ -9,6 +11,27 @@ defmodule UserServiceWeb.Pow.Routes do
 
   @impl true
   def after_sign_in_path(conn) do
-    Routes.external_redirect_path(conn, :show)
+    case external_url(conn) do
+      nil ->
+        "/"
+
+      _ ->
+        Routes.external_redirect_path(conn, :show)
+    end
+  end
+
+  @impl true
+  def after_sign_out_path(conn, routes_module \\ __MODULE__) do
+    case external_url(conn) do
+      nil ->
+        routes_module.session_path(conn, :new)
+
+      _ ->
+        Routes.external_redirect_path(conn, :show)
+    end
+  end
+
+  defp external_url(conn) do
+    Plug.Conn.get_session(conn, RedirectTo.session_key())
   end
 end
